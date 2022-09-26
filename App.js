0ffc0d21a30a7e33a05e7bfcfeb4379754349952
maxwellxiaoyuwu <xiaoyu.wu@northeastern.edu>
2022-09-26 16:00:59 -0700

    commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
+import { useState } from 'react';
+import { Button } from 'react-native';
 
 export default function App() {
-  const name = 'Xiaoyu';
+  const name = 'Welcome to myApp!';
+  const [modelVisible, setModuleVisible] = useState(false);
+  const makeModalVisible = () => {
+    setModuleVisible(true);
+  };
+  const makeModalInVisible = () => {
+    setModuleVisible(false);
+  };
   const onTextAdd = function (newText) {
     console.log(newText);
+    setModuleVisible(false);
   };
   return (
-    <View style={styles.container}>
-      <Header appName={name}></Header>
-      <Input onAdd={onTextAdd}></Input>
-      <Text>Open up App.js to start working on your app!{name}</Text>
+    <SafeAreaView flex="1" style={styles.container}>
+      <View style={styles.topContainer}>
+        <View style={styles.header}>
+          <Header appName={name}></Header>
+        </View>
+
+        <Button title="Add a Goal" onPress={makeModalVisible}></Button>
+      </View>
+      <Input modal={modelVisible} onAdd={onTextAdd} onCancel={makeModalInVisible}></Input>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.text}>You typed...</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -22,7 +40,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  header: {
+    borderWidth: 3,
+    borderColor: 'thistle',
+    borderRadius: 50,
+  },
+  text: {
+    backgroundColor: '#aaa',
+    borderRadius: 5,
+  },
+  topContainer: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  bottomContainer: {
+    flex: 4,
+    alignItems: 'center',
+    backgroundColor: 'purple',
+    padding: 30,
   },
 });
--- components/Input.js
+++ components/Input.js
@@ -1,20 +1,62 @@
-import { View, Text, Button, TextInput } from 'react-native';
+import { View, Text, Button, TextInput, Modal, StyleSheet, Image } from 'react-native';
 import React from 'react';
 import { useState } from 'react';
 
 export default function Input(props) {
   const [text, setText] = useState('');
-  const { onAdd } = props;
+  const { modal, onAdd, onCancel } = props;
   return (
-    <>
-      <TextInput onChangeText={(newText) => setText(newText)} value={text} placeholder="useless placeholder"></TextInput>
-      <Button
-        title="Confirm"
-        onPress={() => {
-          onAdd(text);
-          setText('');
-        }}
-      ></Button>
-    </>
+    <Modal visible={modal}>
+      <View style={styles.container}>
+        <Image style={styles.tinyLogo} source={require('../assets/images/tiny_logo.png')}></Image>
+        <TextInput style={styles.input} placeholder="write your goal here" onChangeText={(newText) => setText(newText)} value={text}></TextInput>
+        <View style={styles.buttonAll}>
+          <View style={styles.button}>
+            <Button
+              title="Confirm"
+              disabled={text.length ? true : false}
+              onPress={() => {
+                onAdd(text);
+                setText('');
+              }}
+            ></Button>
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Cancel"
+              onPress={() => {
+                onCancel();
+                setText('');
+              }}
+            ></Button>
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  input: {
+    color: '#ff00ff',
+    borderBottomWidth: 2,
+    borderBottomColor: 'purple',
+    margin: 5,
+  },
+  button: {
+    margin: 5,
+    width: '30%',
+  },
+  buttonAll: {
+    flexDirection: 'row-reverse',
+  },
+});
